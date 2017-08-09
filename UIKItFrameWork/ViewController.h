2db585bba063629080985df54d9b84ce62d4cd1a
冯学仕 <fengxueshi@fengxueshidemacbook-pro.local>
2017-08-09 15:40:20 +0800

    Initial Commit

---
+++ UIKItFrameWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIKItFrameWork
+//
+//  Created by 冯学仕 on 17/8/9.
+//  Copyright © 2017年 rooten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIKItFrameWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIKItFrameWork
+//
+//  Created by 冯学仕 on 17/8/9.
+//  Copyright © 2017年 rooten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
